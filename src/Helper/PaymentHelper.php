e0ec786073f7bee84b5b17541f6d0eab3ac49f99
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-07-28 09:05:07 +0530

    Update PaymentHelper.php

--- src/Helper/PaymentHelper.php
+++ src/Helper/PaymentHelper.php
@@ -283,32 +283,6 @@ class PaymentHelper
         }
     }
 
-    /**
-     * Update order status by order id
-     *
-     * @param int $orderId
-     * @param float $statusId
-     */
-    public function updateOrderStatus($orderId, $statusId)
-    {
-        try {
-            /** @var \Plenty\Modules\Authorization\Services\AuthHelper $authHelper */
-            $authHelper = pluginApp(AuthHelper::class);
-            $authHelper->processUnguarded(
-                    function () use ($orderId, $statusId) {
-                    //unguarded
-                    $order = $this->orderRepository->findOrderById($orderId);
-
-                    if (!is_null($order) && $order instanceof Order) {
-                        $status['statusId'] = (float) $statusId;
-                        $this->orderRepository->updateOrder($status, $orderId);
-                    }
-                }
-            );
-        } catch (\Exception $e) {
-            $this->getLogger(__METHOD__)->error('Novalnet::updateOrderStatus', $e);
-        }
-    }
 
     /**
      * Get Novalnet status message.
